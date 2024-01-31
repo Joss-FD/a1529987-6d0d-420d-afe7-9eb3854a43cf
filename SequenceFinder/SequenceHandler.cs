3bc1fa738dd70797b6409eb86eafac93085ca06a
Josselin <jossfrancois@gmail.com>
2024-01-31 18:23:56 +1100

    Add comments to SequenceFinder
    Added short descriptive comment to the .NET core logic

--- SequenceFinder/SequenceHandler.cs
+++ SequenceFinder/SequenceHandler.cs
@@ -16,7 +16,7 @@ namespace SequenceFinder
         private List<int>? Parse(string listString)
         {
 
-            //Quick validation
+            //Quick validation to ensure Parsing doesn't fail
             if(string.IsNullOrEmpty(listString) || listString.Any(c => !char.IsDigit(c) && !char.IsWhiteSpace(c)))
             {
                 return null;
@@ -26,18 +26,20 @@ namespace SequenceFinder
             
         }
 
-
+        /// <summary>
+        /// This method takes a list of number and find the largest sequence of incremental number in a row, save its size and index to 
+        /// then return the sequence of number itself as a list. The list is then converted to a string separated by whitespaces.
+        /// </summary>
+        /// <param name="inputString">The list of numbers in string format</param>
+        /// <returns>A string representing the longest incremental sequence</returns>
         public string FindLongestSequence(string inputString)
         {
-            
-            List<int> longestSequence = new List<int>();
             int maxLength = 0;
+            // currLength is set to 1 to include the very first element of the iput list
             int currLength = 1;
             int maxIndex = 0;
 
-
             var numbersList = Parse(inputString);
-
 
             if (numbersList == null || numbersList.Count == 0) 
             {
@@ -48,13 +50,14 @@ namespace SequenceFinder
 
             for (int i = 1; i < size; i++)
             {  
-
+                // Moving through the list comparing the current number with the next number
                 if (numbersList[i] > numbersList[i-1])
                 {
                     currLength++;
                 }    
                 else
                 {
+                    // We keep track of the current largest sequence's size and index
                     if (maxLength < currLength)
                     {
                         maxLength = currLength;
@@ -71,10 +74,8 @@ namespace SequenceFinder
                 maxIndex = size - currLength;
             }
 
-
-            longestSequence = numbersList.GetRange(maxIndex, maxLength);
-
-            var result = String.Join(" ", longestSequence);
+            // Return value as a string seperated by whitespaces
+            var result = String.Join(" ", numbersList.GetRange(maxIndex, maxLength));
             return result;
         }
 
